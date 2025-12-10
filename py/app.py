0965471f904e7c977a8e9d9df8d0899d63b0538b
gvdbroek <141612818+gvdbroek@users.noreply.github.com>
2025-12-10 22:11:20 +0100

    simple impl

---
+++ py/app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return dict(message="Ok")
+
+
+@app.post("/")
+async def routine():
+    return dict(message="Ok")
+    # async with httpx.AsyncClient() as client:
+    #     tasks = [client.get("https://api.example.com/data") for _ in range(3)]
+    #     responses = await asyncio.gather(*tasks)
+    # return [r.json() for r in responses]
