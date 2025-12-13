459b67edcac25ac5b2700394d546c5abaa1ca364
gvdbroek <141612818+gvdbroek@users.noreply.github.com>
2025-12-13 08:48:36 +0100

    added dependencies

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
-}
+use ticks::projects::Project;
+use ticks::tasks::Task;
+use ticks::{AccessToken, TickTick, TickTickError};
+
+#[tokio::main]
+async fn main() -> Result<(), ()> {
+    println!("Collecting environment");
+    println!("Collecting projects");
+    println!("Collecting tasks");
+    println!("Creating tasks for non-actionable projects");
+
+    Ok(())
+Creating tasks for non-actionable projects}
